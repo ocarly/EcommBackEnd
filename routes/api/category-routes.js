758ecca519fc199d7e6719403621abd9ba68758a
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-11-17 16:19:38 -0600

    added more routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,70 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const catDat = await Category.findAll({
+      include: [ Product ]
+    })
+    res.json(catDat)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const catDat = await Category.findOne({
+      where: {id:req.params.id},
+      include: [ Product ]
+    })
+    res.json(catDat)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const catDat = await Category.create(req.body)
+    res.json(catDat)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const catDat = await Category.update(req.body, {
+      where: {id:req.params.id}
+
+    })
+    res.json(catDat)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const catDat = await Category.destroy({
+      where: {id:req.params.id}
+    })
+    res.json(catDat)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // delete a category by its `id` value
 });
 
