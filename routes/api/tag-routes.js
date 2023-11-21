8705a0d7f23d1929ec854bafb4ef8bef69a564a2
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-11-20 21:24:29 -0600

    added get tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,61 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => { 
+  try {
+    const whatever = await Tag.findAll({
+      include: [{
+        model: Product,
+        through: ProductTag
+      }]
+    })
+    res.json(whatever)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
   // delete on tag by its `id` value
 });
 
